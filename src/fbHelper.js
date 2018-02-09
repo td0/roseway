5dafcd96a5b529d6a18548ea8469080125726d90
td0 <tadho@me.com>
2018-02-10 01:48:19 +0700

    add fixed road issues functionalities

--- src/fbHelper.js
+++ src/fbHelper.js
@@ -4,3 +4,11 @@ import fbConfig from '../keystore'
 const app = firebase.initializeApp(fbConfig)
 
 export const db = app.database()
+export const fixedIssueRef = db.ref('ReportFixedIssues')
+export const reportsRef = db.ref('Reports')
+
+let userList
+db.ref('Users').once('value').then(snap => {
+  userList = snap.val()
+})
+export {userList}
--- src/pages/FixedRoad.vue
+++ src/pages/FixedRoad.vue
@@ -1,16 +1,110 @@
 <template>
-  <h1>Fixed Road</h1>
+  <div id="wrapper">
+    <!-- <button @click="testPrint" >Test </button>
+    <br> <br> -->
+
+    <md-dialog :md-active.sync="showDialog" class="image-preview">
+      <img v-bind:src="imagePreviewUrl" >
+    </md-dialog>
+
+    <center v-if="loading">
+      <br/>
+      <md-progress-spinner md-mode="indeterminate" class="md-accent"/>
+    </center>
+
+    <md-table v-else v-model="fixedReports" md-sort="streetName" md-sort-order="asc">
+      <md-table-toolbar>
+        <h1 class="md-title">Fixed Road Issues</h1>
+      </md-table-toolbar>
+
+      <md-table-row slot="md-table-row" slot-scope="{ item }">
+        <md-table-cell md-label="Photo">
+          <img :src="item.imageUrl" height="40" width="40" @click="imagePreview(item.imageUrl)"/>
+        </md-table-cell>
+        <md-table-cell md-label="Street" md-sort-by="streetName">{{ item.streetName }}</md-table-cell>
+        <md-table-cell md-label="Reporter" md-sort-by="reporterName">{{ item.reporterName }}</md-table-cell>
+        <md-table-cell md-label="Issuer">
+          <ul>
+            <li v-for="(issuer, idx) in item.issuers" :key="idx">{{issuer}}</li>
+          </ul>
+        </md-table-cell>
+        <md-table-cell md-label="Action">
+          <md-button class="md-icon-button btn-green">
+            <md-icon>done</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button btn-red">
+            <md-icon>clear</md-icon>
+          </md-button>
+        </md-table-cell>
+      </md-table-row>
+    </md-table>
+
+  </div>
 </template>
 
 <script>
+import {fixedIssueRef, reportsRef, userList} from '../fbHelper'
+// import {userList} from '../fbHelper'
+
 export default {
   name: 'FixedRoad',
   data () {
-    return {}
+    return {
+      showDialog: false,
+      loading: true,
+      imagePreviewUrl: 'https://www.wonderplugin.com/videos/demo-image0.jpg',
+      fixedReports: []
+    }
+  },
+  methods: {
+    testPrint: function () {
+      console.log(this.fixedReports)
+    },
+    imagePreview: function (imgUrl) {
+      this.imagePreviewUrl = imgUrl
+      this.showDialog = true
+    }
+  },
+  mounted: function () {
+    fixedIssueRef.once('value').then(snap1 => {
+      this.fixedReports = []
+      for (let key in snap1.val()) {
+        reportsRef.child(key).once('value').then(snap2 => {
+          let tmpObj = snap2.val()
+          tmpObj.issuers = {}
+          for (let userKey in snap1.val()[key]) {
+            tmpObj.issuers[userKey] = userList[userKey].name
+          }
+          this.fixedReports.push(tmpObj)
+        })
+      }
+      this.loading = false
+    })
+  },
+  beforeDestroy: function () {
+    fixedIssueRef.off()
+    reportsRef.off()
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
+ul {
+  list-style-type: none;
+  padding:0;
+}
+.image-preview {
+  max-width: 400px;
+  img {
+    width: 100%;
+    height: auto;
+  }
+}
+.btn-green .md-icon.md-theme-default.md-icon-font{
+  color: green;
+}
+.btn-red .md-icon.md-theme-default.md-icon-font{
+  color: red;
+}
 </style>
